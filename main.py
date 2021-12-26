37532ce55585eaa5e853a2b6da10b15c06c2de20
Fabion <fampiont@csd.auth.gr>
2021-12-26 15:49:07 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+def gauss(A,b):
+    print()
+
+
+
+print("Μέθοδος PA=LU")
+n=int(input("Ποιά είναι η διάσταση (nxn) του Πίνακα σου?\n"))
+for x in range(1,n+1):
+    for y in range(1,n+1):
